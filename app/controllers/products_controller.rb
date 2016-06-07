6faf1064a5de89d26b68648899a026924f19dec2
chandrasekharkolla <chandrasekharkolla@gmail.com>
2016-06-06 22:25:24 -0500

    Adds styling and all functionality for the products controller and views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,58 @@
 class ProductsController < ApplicationController
+
+	def index
+		@products = Product.all
+	end
+
+	def show
+		@product = Product.find(params[:id])
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+
+		if @product.save
+			flash[:success] = "Product has been added successfully to the database"
+			redirect_to root_url
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		@product = Product.find(params[:id])
+
+		if @product.update_attributes(product_params)
+			flash[:success] = "Product has been updated successfully"
+			redirect_to root_url
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@product = Product.find(params[:id])
+
+		if @product.destroy
+			flash[:success] = "Product has been deleted successfully"
+			redirect_to root_url
+		else
+			flash[:danger] = "Product can not be deleted"
+		end
+	end
+
+
+	private
+
+		def product_params
+			params.require(:product).permit(:title, :description, :image, :price)
+		end
 end

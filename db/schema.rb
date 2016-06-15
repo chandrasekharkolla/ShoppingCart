130512cab4743a4f156b15a700269bde7c21b1f8
chandrasekharkolla <chandrasekharkolla@gmail.com>
2016-06-15 01:38:43 -0500

    Just like that

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607024505) do
+ActiveRecord::Schema.define(version: 20160608185435) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.string   "image"
-    t.decimal  "price",       precision: 8, scale: 2
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.decimal  "price",              precision: 8, scale: 2
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
